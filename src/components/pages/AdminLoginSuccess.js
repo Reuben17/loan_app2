58e008db98464bd050ad80fcf96abb37bcd04265
Reuben <reubenmbalanya@gmail.com>
2022-09-14 17:10:55 +0300

    Frontend Commit

---
+++ src/components/pages/AdminLoginSuccess.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../csspages/AdminLoginSuccess.css';
+
+export default function AdminLoginSuccess(){
+    return (
+    <> 
+    <div className='admin-primary-container'>
+    <div className='admin-secondary-container'>
+    <button >Registor Underwriter</button>
+    <button>View All Loans</button>
+    <button>View Approved Loans</button>
+    <button>View Declined Loans</button>
+    </div>
+    </div>
+    </>
+    );
+    }
+
+
